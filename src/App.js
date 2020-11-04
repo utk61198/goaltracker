03daa28db28446a7d94e2348d9a88541d9000b78
Utkarsh <utk61198@gmail.com>
2020-11-05 00:54:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { signInWithGoogle } from './firebase';
+import {auth} from './firebase';
+
+import { Grid,Button ,makeStyles,Paper,Typography} from '@material-ui/core';
+
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        {
+
+          this.state.currentUser ?
+
+            (
+              <div>
+                <Paper
+                elevation={3}
+                style={{height:"50vh",marginTop:"10%"}}>
+                 <Grid
+  container
+  direction="column"
+  justify="space-between"
+  alignItems="center"
+  style={{marginTop:"10px"}}
+  
+
+>
+                
+                
+                {/* <img src={this.state.currentUser.photoURL} /> */}
+              
+                <Typography variant="h6" component="h2" gutterBottom color="primary">
+
+              Name: {this.state.currentUser.displayName}
+              </Typography>
+              <Typography variant="h6" component="h2" gutterBottom color="primary">
+
+              Email: {this.state.currentUser.email}
+              </Typography>
+
+              <Button variant="outlined" color="primary" onClick={() => auth.signOut()}>LOG OUT</Button>
+              </Grid>
+              </Paper>
+            
+            </div>
+            ) :
+
+           
+               <Grid
+  container
+  direction="column"
+  justify="space-between"
+  alignItems="center"
+  style={{marginTop:"10%"}}
+
+>
+<Typography variant="h2" component="h2" gutterBottom color="primary">
+  Goal Tracker
+</Typography>
+
+
+
+<Paper
+            elevation={3}
+            >
+
+       
+
+  <Button variant="outlined" onClick={signInWithGoogle} color="primary">
+  Sign in with Google
+</Button>
+</Paper>
+
+
+
+</Grid>
+
+             
+           
+
+
+           
+
+        }
+      </div >
+    );
+  }
 }
+
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBflrD8mwCqTFrYNtm3CRRKgSNfjX9nc44",
+    authDomain: "goaltracker-e68d4.firebaseapp.com",
+    databaseURL: "https://goaltracker-e68d4.firebaseio.com",
+    projectId: "goaltracker-e68d4",
+    storageBucket: "goaltracker-e68d4.appspot.com",
+    messagingSenderId: "707982455944",
+    appId: "1:707982455944:web:77b717222ea121cc2fdb3d",
+    measurementId: "G-GGJ6JZHZ97"
+  };
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
